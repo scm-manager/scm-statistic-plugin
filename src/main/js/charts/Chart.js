09a7566bf7320eb33ada64a074323567324dde4a
Maren Süwer <maren.suewer@cloudogu.com>
2019-01-14 08:59:49 +0100

    add dummy modal functionality

--- src/main/js/charts/Chart.js
+++ src/main/js/charts/Chart.js
@@ -1,6 +1,6 @@
 // @flow
 import * as React from "react";
-import { translate } from "react-i18next";
+import {translate} from "react-i18next";
 import injectSheet from "react-jss";
 import classNames from "classnames";
 
@@ -10,29 +10,65 @@ type Props = {
   children: React.Node
 };
 
+type State = {
+  showModal: boolean
+}
+
 const styles = {
   higherMaxColumn: {
-    maxHeight: "225px !important"
+    maxHeight: "225px !important" // ?
+  },
+  pointer: {
+    cursor: "pointer"
   }
 };
 
-class Chart extends React.Component<Props> {
+class Chart extends React.Component<Props, State> {
+
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      showModal: false
+    };
+  }
+
+  showModal = () => { //TODO: does not work yet - guess because this component is called in column environment
+    this.setState({
+      showModal: true
+    })
+  };
+
   render() {
-    const { children, classes } = this.props;
-    //TODO: add link and Title to return!
+    const {children, classes} = this.props;
+    const {showModal} = this.state;
+    //TODO: add link and Title to return! - ?
+
+    let modal = null;
+    if (showModal) {
+      modal = (
+        <div className="modal">
+          <div className="modal-background"></div>
+          <div className="modal-content">
+            {children}
+          </div>
+          <button className="modal-close is-large" aria-label="close"></button>
+        </div>);
+    }
+
     return (
-      <div className={classNames("column is-half", classes.higherMaxColumn)}>
-        <div className="media">
-          <div className="media-content">{children}</div>
-          <div className="media-right">
+      <>
+        {modal}
+        <div className={classNames("column is-half", classes.higherMaxColumn)}>
+          <div className="media">
+            <div className="media-content">{children}</div>
+            <div className={classNames(classes.pointer, "media-right")} onClick={this.showModal}>
             <span className="icon is-small">
-              <a href="">
-                <i className="fas fa-search-plus" />
-              </a>
+                <i className="fas fa-search-plus"/>
             </span>
+            </div>
           </div>
         </div>
-      </div>
+      </>
     );
   }
 }
