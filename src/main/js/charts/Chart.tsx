f12c104c9189da2cb37b6ddd47f3f167d8d794d5
Florian Scholdei <florian.scholdei@cloudogu.com>
2022-01-21 17:10:12 +0100

    Enlarge fullscreen view

--- src/main/js/charts/Chart.tsx
+++ src/main/js/charts/Chart.tsx
@@ -21,63 +21,70 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-import React  from "react";
-import { withTranslation, WithTranslation } from "react-i18next";
-import { ErrorNotification, Loading, Modal, Level, Tooltip, Icon } from "@scm-manager/ui-components";
+import React, { FC, useEffect, useState } from "react";
+import { useTranslation } from "react-i18next";
 import styled from "styled-components";
+import { ErrorNotification, Level, Loading, Notification, OpenInFullscreenButton } from "@scm-manager/ui-components";
 import { StatisticData } from "../DataTypes";
-import NoDataFound from "../NoDataFound";
 
 type RenderProps = {
   statisticData: StatisticData;
   options: any;
 };
 
-type Props = WithTranslation & {
+type Props = {
   render: (props: RenderProps) => any;
   getData: (p: void) => Promise<any>;
   title: string;
 };
 
-type State = {
-  error?: Error;
-  loading: boolean;
-  showModal: boolean;
-  statisticData?: StatisticData;
-};
-
 const ColumnSettings = styled.div`
   max-height: none !important;
   height: unset !important;
-  margin-bottom: 1.5rem;
 `;
 
-const CanvasContainerDiv = styled.div`
-  margin-bottom: 5px;
-  height: 30vh;
+const CanvasModalContainer = styled.div`
+  height: 60vh;
 `;
 
 const CanvasContainerArticle = styled.article`
-  margin-bottom: 5px;
   height: 30vh;
 `;
-class Chart extends React.Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-    this.state = {
-      loading: true,
-      showModal: false
-    };
-  }
 
-  getStatistics() {
-    const { getData } = this.props;
+type ChartObjectProps = {
+  statisticData: StatisticData;
+  render: (props: RenderProps) => any;
+};
+
+const ChartsObject: FC<ChartObjectProps> = ({ statisticData, render }) => {
+  const renderProps: RenderProps = {
+    statisticData: statisticData,
+    options: {
+      maintainAspectRatio: false, // Don't maintain w/h ratio
+      legend: {
+        position: "bottom"
+      }
+    }
+  };
+
+  return render(renderProps);
+};
+
+const Chart: FC<Props> = ({ render, getData, title }) => {
+  const [t] = useTranslation("plugins");
+  const [isLoading, setLoading] = useState(true);
+  const [error, setError] = useState<Error | null>(null);
+  const [statisticData, setStatisticData] = useState<StatisticData | null>(null);
+
+  useEffect(() => {
+    getStatistics();
+  }, []);
+
+  const getStatistics = () => {
     getData().then(result => {
       if (result.error) {
-        this.setState({
-          loading: false,
-          error: result.error
-        });
+        setLoading(false);
+        setError(result.error);
       } else {
         const count = [];
         const value = [];
@@ -86,110 +93,52 @@ class Chart extends React.Component<Props, State> {
           value.push(statisticData.value);
           count.push(statisticData.count);
         }
-        this.setState({
-          statisticData: {
-            value: value,
-            count: count
-          },
-          loading: false
+        setStatisticData({
+          value: value,
+          count: count
         });
+        setLoading(false);
       }
-    });
-  }
-
-  showModal = () => {
-    this.setState({
-      showModal: true
     });
   };
 
-  onClose = () => {
-    this.setState({
-      showModal: false
-    });
-  };
+  let content = null;
 
-  componentDidMount() {
-    this.setState({
-      ...this.state,
-      loading: true
-    });
-    this.getStatistics();
-  }
-
-  createChartsObject() {
-    if (!this.state.statisticData) {
-      return null;
-    }
-
-    const renderProps: RenderProps = {
-      statisticData: this.state.statisticData,
-      options: {
-        maintainAspectRatio: false, // Don't maintain w/h ratio
-        legend: {
-          position: "bottom"
-        }
-      }
-    };
-
-    return <CanvasContainerArticle>{this.props.render(renderProps)}</CanvasContainerArticle>;
-  }
-
-  render() {
-    const { t } = this.props;
-    const { error, loading, statisticData, showModal } = this.state;
-
-    let content = null;
-    let modal = null;
-
-    if (error) {
-      content = <ErrorNotification error={error} />;
-    } else if (loading || !statisticData) {
-      content = <Loading />;
-    } else if (statisticData.value.length === 0 || statisticData.count.length === 0) {
-      content = <NoDataFound />;
-    } else {
-      if (showModal) {
-        modal = (
-          <Modal
-            title={t("scm-statistic-plugin.charts.detailedView")}
-            closeFunction={this.onClose}
-            active={showModal}
-            body={<CanvasContainerDiv className="content">{this.createChartsObject()}</CanvasContainerDiv>}
-          />
-        );
-      }
-
-      return (
-        <ColumnSettings className="column">
-          {modal}
-          <Level
-            left={this.props.title}
-            right={
-              <Tooltip message={t("scm-statistic-plugin.openFullscreen")}>
-                <Icon
-                  name="search-plus"
-                  color="inherit"
-                  onClick={this.showModal}
-                  onEnter={this.showModal}
-                  alt={t("scm-statistic-plugin.openFullscreen")}
-                  tabIndex={0}
-                />
-              </Tooltip>
-            }
-          />
-          {this.createChartsObject()}
-        </ColumnSettings>
-      );
-    }
-
+  if (isLoading || !statisticData) {
+    content = <Loading />;
+  } else if (error) {
+    content = <ErrorNotification error={error} />;
+  } else if (statisticData.value.length === 0 || statisticData.count.length === 0) {
+    content = <Notification type="info">{t("scm-statistic-plugin.noData")}</Notification>;
+  } else {
     return (
-      <ColumnSettings className="column">
-        {this.props.title}
-        {content}
+      <ColumnSettings className="column mb-5">
+        <Level
+          left={title}
+          right={
+            <OpenInFullscreenButton
+              modalTitle={t("scm-statistic-plugin.charts.detailedView")}
+              modalBody={
+                <CanvasModalContainer className="content">
+                  <ChartsObject statisticData={statisticData} render={render} />
+                </CanvasModalContainer>
+              }
+            />
+          }
+        />
+        <CanvasContainerArticle className="mb-2">
+          <ChartsObject render={render} statisticData={statisticData} />
+        </CanvasContainerArticle>
       </ColumnSettings>
     );
   }
-}
 
-export default withTranslation("plugins")(Chart);
+  return (
+    <ColumnSettings className="column">
+      <h3 className="mb-2">{title}</h3>
+      {content}
+    </ColumnSettings>
+  );
+};
+
+export default Chart;
