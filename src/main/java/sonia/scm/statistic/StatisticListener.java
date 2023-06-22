3260fc23153e4e2adb91a281c5893ec91e210173
Eduard Heimbuch <eduard.heimbuch@cloudogu.com>
2023-06-22 11:29:45 +0200

    Fix event listener permission checks
    Handle repository events in administration context since no permission checks should be required for internal operations
    
    Committed-by: Rene Pfeuffer <rene.pfeuffer@cloudogu.com>

--- src/main/java/sonia/scm/statistic/StatisticListener.java
+++ src/main/java/sonia/scm/statistic/StatisticListener.java
@@ -23,17 +23,18 @@
  */
 package sonia.scm.statistic;
 
+import com.github.legman.Subscribe;
 import com.google.inject.Inject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import sonia.scm.EagerSingleton;
 import sonia.scm.HandlerEventType;
-import com.github.legman.Subscribe;
 import sonia.scm.plugin.Extension;
 import sonia.scm.repository.Changeset;
 import sonia.scm.repository.PostReceiveRepositoryHookEvent;
 import sonia.scm.repository.Repository;
 import sonia.scm.repository.RepositoryEvent;
+import sonia.scm.web.security.AdministrationContext;
 
 import java.io.IOException;
 
@@ -45,23 +46,29 @@ import java.io.IOException;
 public class StatisticListener {
   private static final Logger LOG = LoggerFactory.getLogger(StatisticListener.class);
 
+  private final StatisticManager statisticManager;
+
+  private final AdministrationContext administrationContext;
+
   @Inject
-  public StatisticListener(StatisticManager statisticManager) {
+  public StatisticListener(AdministrationContext administrationContext, StatisticManager statisticManager) {
+    this.administrationContext = administrationContext;
     this.statisticManager = statisticManager;
   }
 
   @Subscribe
   public void onHookEvent(PostReceiveRepositoryHookEvent event) {
     LOG.debug("update statistic of repository {}", event.getRepository().getName());
-
-    try (Statistics statistics = statisticManager.get(event.getRepository())) {
-      for (Changeset c : event.getContext().getChangesetProvider().getChangesets()) {
-        statistics.add(c);
+    administrationContext.runAsAdmin(() -> {
+      try (Statistics statistics = statisticManager.get(event.getRepository())) {
+        for (Changeset c : event.getContext().getChangesetProvider().getChangesets()) {
+          statistics.add(c);
+        }
+        statistics.commit();
+      } catch (IOException ex) {
+        LOG.error("could not update statistic", ex);
       }
-      statistics.commit();
-    } catch (IOException ex) {
-      LOG.error("could not update statistic", ex);
-    }
+    });
   }
 
   @Subscribe
@@ -71,10 +78,9 @@ public class StatisticListener {
 
       LOG.trace("receive delete event for repository {}",
         repository.getId());
-      statisticManager.remove(repository);
+      administrationContext.runAsAdmin(() -> {
+        statisticManager.remove(repository);
+      });
     }
-
   }
-
-  private StatisticManager statisticManager;
 }
